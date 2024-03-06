71811e60a6ce7af3a9a5e50a3db5103ca0f57ba0
victord03 <vic.kaklamanis@gmail.com>
2024-03-06 23:36:17 +0200

    Added the project charter.

--- src/main.py
+++ src/main.py
@@ -1,5 +1,26 @@
 """
+App short description:
+The app will be accepting images of receipts that the user will be sending over, scan the information to ensure image
+clarity and usefulness of the content on the receipt, and when the content is deemed satisfactory, award the user with
+a tiny sum of money for each receipt.
 
+Quality assurance:
+The data must contain information such as product bought, quantity, price, company (seller), geographical location,
+along with user data such as age and sex which will be provided via the user account during the account creation.
+
+Business case:
+The business model is that the user is getting paid by our startup company for images of receipts, which in turn is
+sold to data processors as consumer data information or market trends. The incentive for the user will be to submit a
+large number of receipts which can amount to a decent total sum, while for the company will be to understand the types
+of data that are attractive to data processor companies.
+
+Business need:
+Further research is required to determine how data about trends is collected by companies, and the general market chain
+for data gathering and processing of consumer information.
+
+Future plans:
+In the future, our company may or may not perform in-house data analysis, but initially will only sell the raw data in
+bulk.
 
 
 """
