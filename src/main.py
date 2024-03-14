d1bce41f13f38cf3e73cf0843714a3857b3064eb
victord03 <vic.kaklamanis@gmail.com>
2024-03-14 16:12:25 +0200

    Attempting a new execution method. So far no results. Difficutly installing tesseract-ocr library on MacOS.

--- src/main.py
+++ src/main.py
@@ -1,6 +1,8 @@
 import numpy as np
-import pytesseract
 from PIL import Image
+
+
+import pytesseract
 import cv2
 
 
@@ -8,30 +10,67 @@ def main():
 
     file_name = 'image_sample3.jpg'
 
-    # using pillow
-    pillow_image = Image.open(file_name)
-    # data = pytesseract.image_to_data(pillow_image, timeout=10)
-    text = pytesseract.image_to_string(pillow_image, timeout=10)
-    # print('-' * 45, '\nPILLOW lib\n\n', text)
+    # Mention the installed location of Tesseract-OCR in your system
+    pytesseract.pytesseract.tesseract_cmd = '/usr/bin/tesseract or /usr/local/bin/tesseract'
 
-    # using opencv
-    cv_image = cv2.imread(file_name)
+    # Read image from which text needs to be extracted
+    img = cv2.imread(file_name)
 
-    # turn to greyscale
-    grey = cv2.cvtColor(cv_image, cv2.COLOR_BGR2GRAY)
+    # Preprocessing the image starts
 
-    # Gaussian blur to remove some noise
-    blur = cv2.GaussianBlur(grey, (5, 5), 0)
-    bin_img = cv2.adaptiveThreshold(blur, 255, cv2.ADAPTIVE_THRESH_GAUSSIAN_C, cv2.THRESH_BINARY, 31, 2)
+    # Convert the image to gray scale
+    gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
 
-    # Perform dilation and erosion to remove some noise
-    kernel = np.ones((1, 1), np.uint8)
-    img = cv2.dilate(bin_img, kernel, iterations=1)
-    img = cv2.erode(img, kernel, iterations=1)
+    # Performing OTSU threshold
+    ret, thresh1 = cv2.threshold(gray, 0, 255, cv2.THRESH_OTSU | cv2.THRESH_BINARY_INV)
 
-    # text = pytesseract.image_to_string(img)
-    # text = cv2.haveImageReader(file_name)
-    print('-' * 45, '\nOPENCV lib\n\n', text)
+    # Specify structure shape and kernel size.
+    # Kernel size increases or decreases the area
+    # of the rectangle to be detected.
+    # A smaller value like (10, 10) will detect
+    # each word instead of a sentence.
+    rect_kernel = cv2.getStructuringElement(cv2.MORPH_RECT, (18, 18))
+
+    # Applying dilation on the threshold image
+    dilation = cv2.dilate(thresh1, rect_kernel, iterations=1)
+
+    # Finding contours
+    contours, hierarchy = cv2.findContours(dilation, cv2.RETR_EXTERNAL,
+                                           cv2.CHAIN_APPROX_NONE)
+
+    # Creating a copy of image
+    im2 = img.copy()
+
+    # A text file is created and flushed
+    file = open("recognized.txt", "w+")
+    file.write("")
+    file.close()
+
+    # Looping through the identified contours
+    # Then rectangular part is cropped and passed on
+    # to pytesseract for extracting text from it
+    # Extracted text is then written into the text file
+    for cnt in contours:
+        x, y, w, h = cv2.boundingRect(cnt)
+
+        # Drawing a rectangle on copied image
+        rect = cv2.rectangle(im2, (x, y), (x + w, y + h), (0, 255, 0), 2)
+
+        # Cropping the text block for giving input to OCR
+        cropped = im2[y:y + h, x:x + w]
+
+        # Open the file in append mode
+        file = open("recognized.txt", "a")
+
+        # Apply OCR on the cropped image
+        text = pytesseract.image_to_string(cropped)
+
+        # Appending the text into file
+        file.write(text)
+        file.write("\n")
+
+        # Close the file
+        file.close()
 
 
 if __name__ == '__main__':
